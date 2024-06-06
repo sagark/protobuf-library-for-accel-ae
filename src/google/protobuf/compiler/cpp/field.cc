716dd53c1e02d05a2be06a07e0e06262ea6915d7
abejgonzalez <abe.j.gonza@gmail.com>
2024-06-06 14:41:38 -0700

    remove extra log

--- src/google/protobuf/compiler/cpp/field.cc
+++ src/google/protobuf/compiler/cpp/field.cc
@@ -17,7 +17,6 @@
 #include <vector>
 
 #include "absl/log/absl_check.h"
-#include "absl/log/absl_log.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
